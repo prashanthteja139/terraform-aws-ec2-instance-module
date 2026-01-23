6263f10c8af74531b977d72634af2dae065ef065
Prashanth Teja <100507992+prashanthteja139@users.noreply.github.com>
2026-01-23 19:47:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "instance_type" {
     default = "t3.micro"
     type = string
     description = "Instance size"
-
+#setting limits for the instance use to minimize cost 
     validation {
         condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
         error_message = "Valid values for instance_type are: t3.micro, t3.small, t3.medium"
